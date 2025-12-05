77774700f8a76c7db80bfb4a22ae4004778d9d6a
Jawad0Chaudhary <uasif412@gmail.com>
2025-12-05 16:45:02 +0500

    fix import extension

--- auth-server/src/db.ts
+++ auth-server/src/db.ts
@@ -5,7 +5,7 @@
 
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
+import * as schema from "./schema.js";
 
 const connectionString = process.env.DATABASE_URL!;
 
