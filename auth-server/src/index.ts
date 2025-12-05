b948072436a89b346f78343e6867d9595100bbe7
Jawad0Chaudhary <uasif412@gmail.com>
2025-12-06 00:31:41 +0500

    new commit

--- auth-server/src/index.ts
+++ auth-server/src/index.ts
@@ -18,7 +18,7 @@ const PORT = process.env.AUTH_PORT || 3001;
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:8000"],
+    origin: ["http://localhost:3000", "http://localhost:8000","https://hackathon-i-physical-ai-robotics-te.vercel.app/"],
     credentials: true,
   })
 );
