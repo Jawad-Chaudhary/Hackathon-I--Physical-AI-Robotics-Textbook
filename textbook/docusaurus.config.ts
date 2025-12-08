331dbe0529da6cc9ed21e8ed49bac5cc21da6d60
Jawad0Chaudhary <uasif412@gmail.com>
2025-12-08 16:31:42 +0500

    parse error resolved

--- textbook/docusaurus.config.ts
+++ textbook/docusaurus.config.ts
@@ -12,9 +12,9 @@ const config: Config = {
     // // Production URLs - Railway deployments
     authUrl: 'https://hackathon-i-physical-ai-robotics-textbook-production.up.railway.app',
     apiUrl: 'https://clever-respect-production-5202.up.railway.app',
-  //   authUrl: 'http://localhost:3001',
-  //   apiUrl: 'http://localhost:8000'
-  // },
+    //   authUrl: 'http://localhost:3001',
+    //   apiUrl: 'http://localhost:8000'
+  },
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
